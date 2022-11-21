08a621f0346747812c2ee5ca0f38e86467a08edc
Rossano Pablo Pinto <rossano@gmail.com>
2022-11-21 11:23:45 -0300

    First commit

---
+++ hot-topics.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Author: rossano at gmail dot com
+# timestamp: Mon Oct 31 12:13:53 PM -03 2022
+
+## 5 years back
+NAME="netsoft"
+
+rm -f ${NAME}-final-sorted.txt
+echo "Generating results for ${NAME}"
+for year in $(seq 2018 2022); do
+    while read line; do
+	echo $line $year >> ${NAME}-final.txt
+    done < ${NAME}-${year}.txt
+done
+
+sort ${NAME}-final.txt > ${NAME}-final-sorted.txt
+rm ${NAME}-final.txt
